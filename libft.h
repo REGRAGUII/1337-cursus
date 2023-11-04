72f7d753df1fdce135a6b08d781b625733c59470
REGRAGUII <youssef98reg@gmail.com>
2023-11-04 19:11:12 +0100

    libft

--- libft.h
+++ libft.h
@@ -16,7 +16,9 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
-void    *ft_memchr(const void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+
+ char	*strchr(const char *s, int c);
 
 size_t	ft_strlen(const char *s);
 
