864230cda772c37c371c1944f2781110b76fad48
REGRAGUII <youssef98reg@gmail.com>
2023-11-05 18:53:38 +0100

    libft

--- libft.h
+++ libft.h
@@ -21,6 +21,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 
 char	*ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 size_t	ft_strlen(const char *s);
 
