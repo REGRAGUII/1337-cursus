a78e8d35bc5c8db3cfda34f1a318575e60d0e801
REGRAGUII <youssef98reg@gmail.com>
2023-11-06 19:37:02 +0100

    libft

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: yregragu <yregragu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 17:30:19 by yregragu          #+#    #+#             */
-/*   Updated: 2023/11/06 19:29:22 by yregragu         ###   ########.fr       */
+/*   Updated: 2023/11/06 19:35:39 by yregragu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ char	*strdup(const char *s1)
 	char	*dup;
 	size_t	x;
 
-	dup = malloc(sizeof(char) * (ft_strlen(s1) + 1))
+	dup = malloc(sizeof(char) * (ft_strlen(s1) + 1));
 	if (!dup)
-		return (0)
+		return (0);
 	while (*s1 != '\0')
 	{
 		*dup = *s1;
--- libft.h
+++ libft.h
@@ -22,7 +22,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
-char	*strdup(const char *s1)
+char	*strdup(const char *s1);
 
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
