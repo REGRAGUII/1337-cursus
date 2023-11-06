399a7f75b5bd81b1e4d9e6bb112a9027ec881d9a
REGRAGUII <youssef98reg@gmail.com>
2023-11-06 20:44:47 +0100

    libft

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,11 +6,12 @@
 /*   By: yregragu <yregragu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 17:30:19 by yregragu          #+#    #+#             */
-/*   Updated: 2023/11/06 19:45:52 by yregragu         ###   ########.fr       */
+/*   Updated: 2023/11/06 20:44:18 by yregragu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strdup(const char *s1)
 {
@@ -29,3 +30,11 @@ char	*ft_strdup(const char *s1)
 	dup = '\0';
 	return (dup);
 }
+    int main()
+	{
+    char s[] = "hello youssef";
+    printf("%s\n", *ft_strdup(s));
+    printf("%s", *strdup(s));
+    
+      return 0;
+	}
