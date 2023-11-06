cf0ba803422d0728aed2e5922bfe02778d31d542
Youssef Regragui <125485768+REGRAGUII@users.noreply.github.com>
2023-11-06 17:34:25 +0100

    libft

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yregragu <yregragu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/06 17:26:25 by yregragu          #+#    #+#             */
-/*   Updated: 2023/11/06 17:32:31 by yregragu         ###   ########.fr       */
+/*   Created: 2023/11/06 17:30:19 by yregragu          #+#    #+#             */
+/*   Updated: 2023/11/06 17:32:33 by yregragu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t   ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize)
+char	*strdup(const char *s1)
 {
-    
+
 }
--- libft.h
+++ libft.h
@@ -22,7 +22,10 @@ void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*strdup(const char *s1)
 
 size_t	ft_strlen(const char *s);
+size_t 	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
+size_t	ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
 
 # endif
