bfde932024ed1da0ae340fe21739f02fb72ec7ac
REGRAGUII <youssef98reg@gmail.com>
2023-11-06 19:25:52 +0100

    libft

--- libft.h
+++ libft.h
@@ -24,5 +24,7 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 # endif
