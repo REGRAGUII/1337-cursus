ce25869741087dfc445f2a3e3a9cd5181b8435b7
Youssef Regragui <125485768+REGRAGUII@users.noreply.github.com>
2023-11-06 18:50:33 +0100

    libft

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: yregragu <yregragu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 17:30:19 by yregragu          #+#    #+#             */
-/*   Updated: 2023/11/06 17:32:33 by yregragu         ###   ########.fr       */
+/*   Updated: 2023/11/06 18:50:23 by yregragu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 char	*strdup(const char *s1)
 {
+	char	*dup;
+	size_t	x;
 
+	dup = malloc(sizeof(char) * (ft_strlen(s1) + 1))
+	if (!dup)
+		return (0)
+	while (*s1 != '\0')
+	{
+		*dup = *s1;
+		dup++;
+		s1++;
+	}
+	dup = '\0';
+	return (dup);
 }
