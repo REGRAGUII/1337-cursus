e06a9eb39b3427e7cf1c22b85204183a677ab8b4
REGRAGUII <youssef98reg@gmail.com>
2023-12-01 18:39:03 +0100

    libft

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: yregragu <yregragu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 17:43:51 by yregragu          #+#    #+#             */
-/*   Updated: 2023/11/30 20:13:03 by yregragu         ###   ########.fr       */
+/*   Updated: 2023/12/01 18:39:03 by yregragu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*new_lst;
+	t_list	*ele;
+
+	new_lst = NULL;
+	while (lst && f)
+	{
+		ele = ft_lstnew(f(lst->content));
+		if (!ele)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, ele);
+		lst = lst->next;
+	}
+	return (new_lst);
 }
