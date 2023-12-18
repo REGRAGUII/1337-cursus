faf3f7abe451050353903d1c18e13113e27f672a
REGRAGUII <youssef98reg@gmail.com>
2023-12-18 21:06:01 +0100

    printf

--- Printf/ft_printf.c
+++ Printf/ft_printf.c
@@ -3,12 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yregragu <yregragu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ryuuk_reg <ryuuk_reg@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 16:32:45 by yregragu          #+#    #+#             */
-/*   Updated: 2023/12/15 16:33:01 by yregragu         ###   ########.fr       */
+/*   Updated: 2023/12/18 17:23:31 by ryuuk_reg        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
+int ft_printf(const char *format, ...)
+{
+	va_list args;
+	size_t  len;
+
+	len = 0;
+	while (*format)
+	{
+		if (*format == '%')
+			format++;
+			ft_checker(*format, len);
+		else
+			ft_putchar(*format);
+		format++;
+	}
+}
