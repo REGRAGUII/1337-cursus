3b8c10676ba9429de7062376aa1c6d152d9090a1
REGRAGUII <youssef98reg@gmail.com>
2023-12-20 19:38:32 +0100

    push mac

--- Printf/ft_printf.c
+++ Printf/ft_printf.c
@@ -3,28 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryuuk_reg <ryuuk_reg@student.42.fr>        +#+  +:+       +#+        */
+/*   By: yregragu <yregragu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 16:32:45 by yregragu          #+#    #+#             */
-/*   Updated: 2023/12/18 17:23:31 by ryuuk_reg        ###   ########.fr       */
+/*   Updated: 2023/12/20 18:26:25 by yregragu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
+static void ft_flag_checker(va_list args, char c, int *len)
+{
+	if (c == '%')
+		ft_putchar(c, len)
+	else if (c == 'c')
+		ft_putchar(va_arg(args, int), *len);
+	else if (c == 's')
+		ft_putstr(va_arg(args, char*), *len);
+	else if (c == 'u')
+		ft_putnbr(va_arg(args, unsigned int), *len);
+	else if (c == 'i' || c == 'd')
+		ft_putnbr(va_arg(args, int), *len);
+	else if (c == 'x' || c == 'X')
+		ft_putnbr_base(va_arg(args, unsigned int), *len);
+	else if (c == 'p')
+		ft_putnbr
+}
+
 int ft_printf(const char *format, ...)
 {
-	va_list args;
-	size_t  len;
+	va_list	args;
+	int	len;
 
 	len = 0;
+    va_start(args, format);
 	while (*format)
 	{
 		if (*format == '%')
+		{
 			format++;
-			ft_checker(*format, len);
+			ft_flag_checker(args, *format, &len);
+		}
 		else
 			ft_putchar(*format);
 		format++;
-	}
+    }
+    va_end(args);
+    return (len);
 }
